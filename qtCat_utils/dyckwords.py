e02ac301829f8ade68191ba9ede090da046b2b6c
cocoxhuang <xiaoyuu.huang@gmail.com>
2025-05-12 15:28:19 -0400

    added functionality of prediting multiple statistics such as area_dinv

--- qtCat_utils/dyckwords.py
+++ qtCat_utils/dyckwords.py
@@ -61,3 +61,59 @@ def bounce_data(n, filename):
         file.write('\n'.join(map(str, vectors)))
     print(f"Success. Dyck words of length {n} and their bounce are saved in {filename}.")
     print(f"Time taken to generate data: {time.time() - start} seconds.")
+
+def area_dinv_data(n, filename):
+    '''
+    n : int, the length of Dyck words
+    filename : str, the name of the file to save the data
+    '''
+
+    start = time.time()
+    # All of .area(), .dinv(), .bounce(), and .area_dinv_to_bounce_area_map() come from the sage.combinat.dyck_word module
+    vecs = DyckWords(n)
+    print(f"Retrieving Dyck words of semilength {n}...")
+
+    # File with Dyck vector and Area,Dinv pair
+    vectors = [[list(entry), entry.area(), entry.dinv()] for entry in vecs]
+    # Write list to file
+    with open(filename, 'w') as file:
+        file.write('\n'.join(map(str, vectors)))
+    print(f"Success. Dyck words of length {n} and their area,dinv are saved in {filename}.")
+    print(f"Time taken to generate data: {time.time() - start} seconds.")
+
+def bounce_area_data(n, filename):
+    '''
+    n : int, the length of Dyck words
+    filename : str, the name of the file to save the data
+    '''
+
+    start = time.time()
+    # All of .area(), .dinv(), .bounce(), and .area_dinv_to_bounce_area_map() come from the sage.combinat.dyck_word module
+    vecs = DyckWords(n)
+    print(f"Retrieving Dyck words of semilength {n}...")
+
+    # File with Dyck vector and Area,Dinv pair
+    vectors = [[list(entry), entry.bounce(), entry.area()] for entry in vecs]
+    # Write list to file
+    with open(filename, 'w') as file:
+        file.write('\n'.join(map(str, vectors)))
+    print(f"Success. Dyck words of length {n} and their bounce,area are saved in {filename}.")
+    print(f"Time taken to generate data: {time.time() - start} seconds.")
+
+def area_dinv_bounce_data(n, filename):
+    '''
+    n : int, the length of Dyck words
+    filename : str, the name of the file to save the data
+    '''
+    start = time.time()
+    # All of .area(), .dinv(), .bounce(), and .area_dinv_to_bounce_area_map() come from the sage.combinat.dyck_word module
+    vecs = DyckWords(n)
+    print(f"Retrieving Dyck words of semilength {n}...")
+
+    # File with Dyck vector and Area,Dinv pair
+    vectors = [[list(entry), entry.area(), entry.dinv(), entry.bounce()] for entry in vecs]
+    # Write list to file
+    with open(filename, 'w') as file:
+        file.write('\n'.join(map(str, vectors)))
+    print(f"Success. Dyck words of length {n} and their area,dinv,bounce are saved in {filename}.")
+    print(f"Time taken to generate data: {time.time() - start} seconds.")
